07993225a8eea5f2c5ac8e1ee764f44ed93bcaf7
DESKTOP-QE6O75P\harshita <vchauhan@syptus.com>
2017-06-05 21:34:01 +0530

    test4

--- js/load_more_tweets.js
+++ js/load_more_tweets.js
@@ -1,27 +1,27 @@
 $('document').ready(function(){ 
 
 	$('body').on('click','#load_more_hashtags', function(e){
-		$(".ht_loader").removeClass('display_none');
-		//e.preventDefault();
+		e.preventDefault(); //stops page reloading with ajax calls
+		$(".ht_loader").removeClass('display_none'); /*Show loader*/
 		$.ajax({
 			url : 'load_more_hashtags.php',
 			success: function(data){ 
-				$(".load_more_hashtags").prepend(data);	
-				$(".ht_loader").addClass('display_none');
-				$('#load_more_hashtags').unbind('click');
+				$(".load_more_hashtags").prepend(data);	/*Prepending data*/
+				$(".ht_loader").addClass('display_none'); /*Hide Loader*/
+				//$('#load_more_hashtags').unbind('click');
 			}
 		});
 	});	
 
 	$('body').on('click','#load_more_rt_tweets', function(e){
-		$(".rt_loader").removeClass('display_none');
-		//e.preventDefault();
+		e.preventDefault(); //stops page reloading with ajax calls
+		$(".rt_loader").removeClass('display_none'); /*Show loader*/
 		$.ajax({
 			url : 'load_more_rt_tweets.php',
 			success: function(data){ 
-				$(".load_more_rt_tweets").prepend(data);	
-				$(".rt_loader").addClass('display_none');
-				$('#load_more_rt_tweets').unbind('click');
+				$(".load_more_rt_tweets").prepend(data); /*Prepending data*/	
+				$(".rt_loader").addClass('display_none'); /*Hide Loader*/
+				//$('#load_more_rt_tweets').unbind('click');
 			}
 		});
 	});
